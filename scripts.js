29098918d2f91be8ddb09ee3f283748d2effa7d9
3legggedcat <124866312+3legggedcat@users.noreply.github.com>
2025-02-28 16:37:28 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,11 +1,25 @@
 function selectWorkout(workout) {
-    alert("You selected: " + workout);
+  alert("You selected: " + workout);
 }
 
 function customWorkout() {
-    let workout = prompt("Enter your custom workout:");
-    if (workout) {
-        alert("You created: " + workout);
-    }
+  let workout = prompt("Enter your custom workout:");
+  if (workout) {
+    alert("You created: " + workout);
+  }
 }
 // Code for bodytest.html
+
+document.querySelectorAll(".questionsButtons").forEach((element) => {
+  const questionParentNode = element.parentNode;
+  const listNode = `.${questionParentNode.classList[0]}-button`;
+  document.querySelectorAll(listNode).forEach((listElement) => {
+    listElement.addEventListener("click", function () {
+      document.querySelectorAll(listNode).forEach((listButton) => {
+        listButton.classList.remove("selected");
+      });
+      this.classList.add("selected");
+      console.log(this);
+    });
+  });
+});
