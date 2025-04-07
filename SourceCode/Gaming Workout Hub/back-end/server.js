fe4cfa8692ccf8ddec085b9ee2e9418e3a5ecf04
lds295 <lds295@nau.edu>
2025-04-07 16:22:07 -0700

    Add files via upload

---
+++ SourceCode/Gaming Workout Hub/back-end/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Build the absolute path to the front-end folder
+// Adjust the relative path as needed
+const frontEndPath = path.join(__dirname, "../../front-end/HTML+CSS");
+
+// Serve static files from the front-end folder (if you have CSS, JS, etc.)
+app.use(express.static(frontEndPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(frontEndPath, "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
